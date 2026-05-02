74e1d985674b4858a6bbe57df63aae70970d8675
tushar dogne <tushardogne123@gmail.com>
2026-05-03 02:00:26 +0530

    Removed Unused files

--- animation.py
+++
@@ -1,2 +0,0 @@
-import turtle ​ window = turtle.Screen() window.bgcolor("black") ​ # Whole pumpkin pumpkin = turtle.Turtle() pumpkin.hideturtle() pumpkin.color("orange") pumpkin.dot(200) ​ # The turtle to "carve" the pumpkin 
-carver = turtle.Turtle() ​
